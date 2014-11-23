71b59b0df37b7c4361187da4048e44dcce9b5e84
upperstream <developer@upperstream.jp>
2014-11-24 02:32:54 +0900

    Refactory.

--- centos/centos-6.5-minimal/provision_vagrant.sh
+++ centos/centos-6.5-minimal/provision_vagrant.sh
@@ -1,4 +1,9 @@
-mkdir -pm 700 /home/vagrant/.ssh
+yum -y install sudo-1.8.6p3-12.el6
+useradd vagrant
+echo "vagrant" | passwd --stdin vagrant
+echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
+chmod 0440 /etc/sudoers.d/vagrant
+mkdir -p /home/vagrant/.ssh
 curl -kL 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' >> /home/vagrant/.ssh/authorized_keys
-chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
+chmod -R og-rwx /home/vagrant/.ssh
--- centos/centos-6.5-minimal/provision_virtualbox.sh
+++ centos/centos-6.5-minimal/provision_virtualbox.sh
@@ -1,16 +1,18 @@
 KERNEL_VERSION=$(uname -r)
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+VBOX_VERSION=$(cat /root/.vbox_version)
 
 rpm -iv http://ftp.iij.ad.jp/pub/linux/centos/6.5/os/x86_64/Packages/kernel-headers-$KERNEL_VERSION.rpm
 rpm -iv http://ftp.iij.ad.jp/pub/linux/centos/6.5/os/x86_64/Packages/kernel-devel-$KERNEL_VERSION.rpm
 
-yum -y install yum-plugin-priorities
+yum -y install yum-plugin-priorities-1.1.30-30.el6
 rpm -U http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-yum -y --enablerepo epel install dkms
-yum -y groupinstall "Development Tools"
+yum -y --enablerepo epel install dkms-2.2.0.3-28.git.7c3e7c5.el6
+yum -y install gcc-4.4.7-11.el6 make-3.81-20.el6 perl-5.10.1-136.el6_6.1
 
 cd /tmp
-mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /root/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
+rm -rf /root/VBoxGuestAdditions_*.iso
+
+yum -y erase gcc
