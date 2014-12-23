850922e63133abf3c7297eff75a770325435cc8d
upperstream <developer@upperstream.jp>
2014-12-24 01:03:45 +0900

    Corrects Xubuntu provisioners.

--- ubuntu/provisioners/virtualbox.sh
+++ ubuntu/provisioners/virtualbox.sh
@@ -1,11 +1,11 @@
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 
-apt-get insatll -y build-essential
-apt-get install -y kernel-devel-`uname -r` kernel-headers-`uname -r`
+apt-get install -y build-essential
+apt-get install -y linux-headers-`uname -r`
+apt-get install -y dkms
 
 cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
-
