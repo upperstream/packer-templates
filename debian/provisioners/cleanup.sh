76d0e07c6ef549fea52ecbae220d4375c86c0fa0
upperstream <developer@upperstream.jp>
2015-03-07 15:54:22 +0900

    Adds templates for Debian 8.0.

---
+++ debian/provisioners/cleanup.sh
@@ -0,0 +1,1 @@
+test -f /root/.ssh/authorized_keys && sed -i '/^ssh-rsa.*vagrant insecure public key$/d' /root/.ssh/authorized_keys
--- debian/provisioners/virtualbox.sh
+++ debian/provisioners/virtualbox.sh
@@ -1,3 +1,4 @@
+test -z "$VBOX_VER" && VBOX_VER=`cat .vbox_version`
 wget http://download.virtualbox.org/virtualbox/$VBOX_VER/VBoxGuestAdditions_$VBOX_VER.iso
 mount -t iso9660 VBoxGuestAdditions_$VBOX_VER.iso /media/cdrom0
 yes | /media/cdrom0/VBoxLinuxAdditions.run --nox11 || (cat /var/log/vboxadd-install.log && cat /var/lib/dkms/vboxguest/$VBOX_VER/build/make.log)
