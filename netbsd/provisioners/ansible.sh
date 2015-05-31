7bf9aed7890a00e1620e725327d2b67eecdcf6b4
upperstream <developer@upperstream.jp>
2015-05-31 18:46:22 +0900

    Adds NetBSD templates with Ansible.

---
+++ netbsd/provisioners/ansible.sh
@@ -0,0 +1,5 @@
+pkg_add \
+	libyaml-0.1.6nb1 py27-yaml-3.11 \
+	gmp-6.0.0a py27-crypto-2.6.1nb1 py27-expat-2.7.9 py27-setuptools-14.2 py27-ecdsa-0.11 py27-paramiko-1.15.2 \
+	py27-markupsafe-0.23 py27-jinja2-2.7.3 \
+	ansible-1.6.10
---
+++ netbsd/provisioners/python27.sh
@@ -0,0 +1,1 @@
+pkg_add libffi-3.2.1 python27-2.7.9nb1
