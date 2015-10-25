476299f4fa72622f9b04e20cea99726a057f73a2
upperstream <developer@upperstream.jp>
2015-10-25 12:47:12 +0900

    Added templates for Ubuntu-15.10. (Resolves #8)

---
+++ ubuntu/provisioners/cleanup.sh
@@ -0,0 +1,1 @@
+rm -f /home/vagrant/*
--- ubuntu/provisioners/virtualbox.sh
+++ ubuntu/provisioners/virtualbox.sh
@@ -1,11 +1,12 @@
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+test -z "$VIRTUALBOX_VERSION" && VIRTUALBOX_VERSION=`cat /root/.vbox_version`
+wget http://download.virtualbox.org/virtualbox/$VIRTUALBOX_VERSION/VBoxGuestAdditions_$VIRTUALBOX_VERSION.iso
 
 apt-get install -y build-essential
 apt-get install -y linux-headers-`uname -r`
 apt-get install -y dkms
 
 cd /tmp
-mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VIRTUALBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
