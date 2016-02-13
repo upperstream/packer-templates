a7865d41c3363aaee14ec8edfe65a324d077ab47
upperstream <developer@upperstream.jp>
2016-02-13 20:09:23 +0900

    Added Debian 7.9 templates. (Resolves #12)

--- debian/provisioners/virtualbox.sh
+++ debian/provisioners/virtualbox.sh
@@ -3,3 +3,5 @@ wget http://download.virtualbox.org/virtualbox/$VBOX_VER/VBoxGuestAdditions_$VBO
 mount -t iso9660 VBoxGuestAdditions_$VBOX_VER.iso /media/cdrom0
 yes | /media/cdrom0/VBoxLinuxAdditions.run --nox11 || (cat /var/log/vboxadd-install.log && cat /var/lib/dkms/vboxguest/$VBOX_VER/build/make.log)
 rm -f VBoxGuestAdditions_*.iso
+shutdown -r now
+sleep 60
