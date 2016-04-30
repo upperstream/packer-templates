7effadeb6e99ba906d694fd955905ba151864e84
upperstream <developer@upperstream.jp>
2016-05-01 00:42:46 +0900

    Updated Alpine Linux 3.3.3 templates: - Memory size and disk size are configurable by user defined variables. - Waits for network getting ready. - Corrected docker provisioning. - Virtual machines on VMware are connected via bridged type network. - Uses open-vm-tools.

--- alpine/provisioners/docker.sh
+++ alpine/provisioners/docker.sh
@@ -1,7 +1,11 @@
-sed -i 's/#http://dl-6\.alpinelinux\.org/alpine/edge/community/http://dl-6.alpinelinux.org/alpine/edge/community' /etc/apk/repositories
-if ! ( grep -q "^http://dl-6\.alpinelinux\.org/alpine/edge/community" /etc/apk/repositories ); then
-  echo "http://dl-6.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
-  echo "Updated /etc/apk/repositories"
+sed -i '/edge\/community/s/^#//' /etc/apk/repositories
+sed -i '/edge\/main/s/^#//' /etc/apk/repositories
+HOST=$(grep '/main$' /etc/apk/repositories | head -1)
+if ! ( grep -q "^http.*/edge/main$" /etc/apk/repositories ); then
+  echo $HOST | sed 's%\(.*\)/\(.*\)/main$%\1/edge/main%' >> /etc/apk/repositories
+fi
+if ! ( grep -q "^http.*/edge/community" /etc/apk/repositories ); then
+  echo $HOST | sed 's%\(.*\)/\(.*\)/main$%\1/edge/community%' >> /etc/apk/repositories
 fi
 apk --update add docker
 rc-update add docker boot
---
+++ alpine/provisioners/vmware.sh
@@ -0,0 +1,2 @@
+apk add open-vm-tools
+rc-update add open-vm-tools default
