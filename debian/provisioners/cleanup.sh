39007ea4427f4755f68d8abd87df043c546ee73a
upperstream <developer@upperstream.jp>
2016-05-12 18:21:01 +0900

    Removed Vagrant's insecure key for Debian provisioning.

--- debian/provisioners/cleanup.sh
+++ debian/provisioners/cleanup.sh
@@ -1,2 +1,3 @@
 test -f /root/.ssh/authorized_keys && sed -i '/^ssh-rsa.*vagrant insecure public key$/d' /root/.ssh/authorized_keys
+sed -i -e '/^PermitRootLogin yes/s/yes/no/' -e '/^PasswordAuthentication yes/s/yes$/no/' /etc/ssh/sshd_config
 rm -f /root/VBoxGuestAdditions*
