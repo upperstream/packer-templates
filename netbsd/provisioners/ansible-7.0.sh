479eec0206388271eb9de28f1ff7d6da4111ba0d
upperstream <developer@upperstream.jp>
2016-05-16 22:24:58 +0900

    NetBSD 7.0: Upgraded Ansible to 2.1.0.0.  Added Testinfra 1.2.0.

--- netbsd/provisioners/ansible-7.0.sh
+++ netbsd/provisioners/ansible-7.0.sh
@@ -1,5 +1,3 @@
-pkg_add \
-	libyaml-0.1.6nb1 py27-yaml-3.11 \
-	gmp-6.0.0a py27-crypto-2.6.1nb1 py27-expat-2.7.10 py27-setuptools-18.3.1 py27-ecdsa-0.13 py27-paramiko-1.15.2 \
-	py27-markupsafe-0.23 py27-jinja2-2.7.3 \
-	ansible-1.6.10nb1
+#!/bin/sh -x
+pkg_add python27-2.7.11 py27-pip-7.1.2
+pip2.7 install ansible==2.1.0.0 testinfra==1.2.0
