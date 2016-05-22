58d0e15347efef07c609c67f9450c5cc41f22000
upperstream <developer@upperstream.jp>
2016-05-22 23:45:18 +0900

    Debian: Added a provisioning script for Ansible.

---
+++ debian/provisioners/ansible.sh
@@ -0,0 +1,5 @@
+apt-get install -y python-pip
+apt-get install -y python-dev -ython-paramiko -ython-jinja2 pythjon-yaml python-six
+pip install -q ansible
+apt-get install -y python-pytest
+pip install -q testinfra
