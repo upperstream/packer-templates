695e9122c98c17990f714e95ce77eb53506eb74d
upperstream <developer@upperstream.jp>
2016-06-19 17:10:23 +0900

    Alpine: Added Testinfra to Ansible provisioner.

--- alpine/provisioners/ansible.sh
+++ alpine/provisioners/ansible.sh
@@ -2,5 +2,5 @@ apk add python py-pip python-dev
 apk add alpine-sdk
 apk add libffi openssl
 apk add libffi-dev openssl-dev
-pip install ansible
+pip install ansible==2.1.0.0 testinfra==1.2.0
 apk del alpine-sdk libffi-dev openssl-dev python-dev
