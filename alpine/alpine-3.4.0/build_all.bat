56536af3401c1189647866718a3c5f522d28e6ee
upperstream <developer@upperstream.jp>
2016-07-24 21:47:47 +0900

    Alpine: Add templates for Alpine Linux 3.4.1

--- alpine/alpine-3.4.0/build_all.bat
+++ alpine/alpine-3.4.0/build_all.bat
@@ -1,6 +1,6 @@
-for %%f in (vars-alpine-3.4.0-*.json) do (
-  for %%g in (alpine-3.4.0-minimal.json alpine-3.4.0-ansible.json) do (
+for %%f in (vars-alpine-3.4-*.json) do (
+  for %%g in (alpine-3.4-minimal.json alpine-3.4-ansible.json) do (
     packer build -var-file=%%f %* %%g
   )
 )
-packer build -var-file=vars-alpine-3.4.0-x86_64.json %* alpine-3.4.0-docker.json 
+packer build -var-file=vars-alpine-3.4-x86_64.json %* alpine-3.4-docker.json 
--- alpine/alpine-3.4.0/build_all.sh
+++ alpine/alpine-3.4.0/build_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-for f in vars-alpine-3.4.0-*.json; do
-  for g in alpine-3.4.0-[am]*.json; do
+for f in vars-alpine-3.4-*.json; do
+  for g in alpine-3.4-[am]*.json; do
     packer build -var-file=$f $* $g
   done
 done
-packer build -var-file=vars-alpine-3.4.0-x86_64.json $* alpine-3.4.0-docker.json 
+packer build -var-file=vars-alpine-3.4-x86_64.json $* alpine-3.4-docker.json 
