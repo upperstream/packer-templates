50c1247d3751201b32a70a939f18120804c2f3e4
upperstream <developer@upperstream.jp>
2016-08-12 03:02:53 +0900

    NetBSD 6.1.5: Add templates for ESXi provider.
    This change also parameterise the values for SSH user/password
    during build time and Vagrant user/password during run time.

--- netbsd/provisioners/vagrant.sh
+++ netbsd/provisioners/vagrant.sh
@@ -1,12 +1,12 @@
-useradd -m vagrant
-usermod -p `pwhash vagrant` -G wheel vagrant
-mkdir -p /home/vagrant/.ssh
+useradd -m $VAGRANT_USER
+usermod -p `pwhash $VAGRANT_PASSWORD` -G wheel $VAGRANT_USER
+mkdir -p /home/$VAGRANT_USER/.ssh
 pkg_add wget-1.16.3
-wget --no-check-certificate -O - https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub >> /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:users /home/vagrant     
-chmod -R og-rwx /home/vagrant/.ssh
+wget --no-check-certificate -O - https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub >> /home/$VAGRANT_USER/.ssh/authorized_keys
+chown -R $VAGRANT_USER /home/$VAGRANT_USER     
+chmod -R og-rwx /home/$VAGRANT_USER/.ssh
 sed \
   -e '/^#UseDNS /s/^#//' \
   -e '/^UseDNS /s/ yes$/ no/' /etc/ssh/sshd_config > /tmp/sshd_config
 mv /tmp/sshd_config /etc/ssh/sshd_config
-echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >> /usr/pkg/etc/sudoers.d/vagrant
+echo "$VAGRANT_USER ALL=(ALL) NOPASSWD:ALL" >> /usr/pkg/etc/sudoers.d/$VAGRANT_USER
