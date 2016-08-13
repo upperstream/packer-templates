8f636c7e2118f022d18b62b39672dec10d186abb
upperstream <developer@upperstream.jp>
2016-08-13 22:21:39 +0900

    NetBSD 7.0: Parameterise SSH user/password for build
    This also parameterise the user name and password for Vagrant user.

--- netbsd/provisioners/vmware-7.0.sh
+++ netbsd/provisioners/vmware-7.0.sh
@@ -3,9 +3,10 @@
 
 test -x /usr/bin/cvs || pkg_add cvs-1.12.13nb2
 
-echo "$(date): Checking out anoncvs@anoncvs.NetBSD.org:/cvsroot at pkgsrc-2015Q4."
+echo "$(date): Checking out anoncvs@anoncvs.NetBSD.org:/cvsroot with a tag \"pkgsrc-2015Q4\"."
 echo "This can take very long (more than 30 minutes)."
 cd /usr && cvs -q -z2 -d anoncvs@anoncvs.NetBSD.org:/cvsroot checkout -r pkgsrc-2015Q4 -P pkgsrc > /dev/null
+echo "$(date): Checkout completed"
 
 pkg_add digest-20121220 libtool-base-2.4.2nb10 automake-1.15nb2 pkg-config-0.29 libdnet-1.12nb1 icu-56.1
 
