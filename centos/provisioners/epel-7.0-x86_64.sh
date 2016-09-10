248c0d93b17307d9aa1ad0db0154514923882a5f
upperstream <developer@upperstream.jp>
2016-09-10 13:38:24 +0900

    CentOS 7.0: Change download urls (#40)

--- centos/provisioners/epel-7.0-x86_64.sh
+++ centos/provisioners/epel-7.0-x86_64.sh
@@ -1,6 +1,6 @@
+#!/bin/sh -ex
 # EPEL
-yum -y install yum-plugin-priorities-1.1.31-25.el7_0
-if ! (md5sum epel-release-7-5.noarch.rpm | grep 8c2d7a86160d3f55f8d45730d325d0a6); then
-  curl -O http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+if ! (md5sum epel-release-7-8.noarch.rpm | grep 8c2d7a86160d3f55f8d45730d325d0a6); then
+  curl -O http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
 fi
-rpm -ivh epel-release-7-5.noarch.rpm
+rpm -ivh epel-release-7-8.noarch.rpm
--- centos/provisioners/virtualbox_dependencies-7.0.sh
+++ centos/provisioners/virtualbox_dependencies-7.0.sh
@@ -1,1 +1,1 @@
-yum -y install bzip2
+yum -y install bzip2-1.0.6-12.el7
