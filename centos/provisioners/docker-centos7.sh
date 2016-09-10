d19bf9794c22a1873ea007b87e1842a47e07e4fd
upperstream <developer@upperstream.jp>
2016-09-10 13:41:05 +0900

    CentOS 7.0: Upgrade Docker to 1.6.0 (#40)

--- centos/provisioners/docker-centos7.sh
+++ centos/provisioners/docker-centos7.sh
@@ -1,10 +1,7 @@
-yum -y install xz-5.1.2-8alpha.el7
-yum -y install systemd-208-11.el7_0.6
-yum -y install glibc-2.17-55.el7_0.3
-yum -y install sqlite-3.7.17-4.el7
-yum -y install device-mapper-libs-1.02.84-14.el7
-yum -y install bash-4.2.45-5.el7_0.4
-yum -y install docker-1.3.2-4.el7.centos
+#!/bin/sh -ex
+yum -y install xz-5.1.2-8alpha.el7 glibc-2.17-55.el7_0.3 sqlite-3.7.17-4.el7 device-mapper-libs-1.02.84-14.el7
+cd /; curl https://get.docker.com/builds/Linux/x86_64/docker-1.6.0.tgz | tar zxvf -
+groupadd docker
 usermod -a -G docker vagrant
-curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
