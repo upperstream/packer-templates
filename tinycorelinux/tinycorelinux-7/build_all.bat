b83d1e0953f79290e60d41a49fb5525456179dfa
upperstream <developer@upperstream.jp>
2016-09-15 22:20:41 +0900

    TinyCoreLinux: Rename tinycorelinux-7.1 to tinycorelinux-7 (#39)

--- tinycorelinux/tinycorelinux-7/build_all.bat
+++ tinycorelinux/tinycorelinux-7/build_all.bat
@@ -1,10 +1,10 @@
 for %%f in (vars-core*.json) do (
-  for %%g in (tc-7.0-ansible.json tc-7.0-compiletc.json tc-7.0-minimal.json) do (
+  for %%g in (tc-7-ansible.json tc-7-compiletc.json tc-7-minimal.json) do (
     packer build -var-file=%%f %%g
   )
 )
 for %%f in (vars-tinycore*.json) do (
-  for %%g in (tc-7.0-*x11.json) do (
+  for %%g in (tc-7-*x11.json) do (
     packer build -var-file=%%f %%g
   )
 )
