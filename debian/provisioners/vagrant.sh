ebdb8e3db018e19b9871a7e3f0c7a5f215731ba0
upperstream <developer@upperstream.jp>
2016-09-20 00:46:58 +0900

    Debian: Add templates for Debian 7.11 (#42)

--- debian/provisioners/vagrant.sh
+++ debian/provisioners/vagrant.sh
@@ -1,3 +1,4 @@
+#!/bin/sh -ex
 mkdir -pm 700 /home/vagrant/.ssh
 curl -kL 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' >> /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
--- debian/provisioners/virtualbox.sh
+++ debian/provisioners/virtualbox.sh
@@ -1,3 +1,4 @@
+#!/bin/sh -ex
 test -z "$VBOX_VER" && VBOX_VER=`cat /root/.vbox_version`
 wget http://download.virtualbox.org/virtualbox/$VBOX_VER/VBoxGuestAdditions_$VBOX_VER.iso
 mount -t iso9660 VBoxGuestAdditions_$VBOX_VER.iso /media/cdrom0
