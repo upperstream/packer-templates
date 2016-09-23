4bcc07f03c5d1647325d2235e67083d54e46486c
upperstream <developer@upperstream.jp>
2016-09-23 12:59:12 +0900

    Alpine 3.4.2: Rename alpine-3.4 to alpine-3.4.2 (#38)
    Template files `*-3.4-*.json` are also renamed to `*-3.4.2-*.json`.

--- alpine/alpine-3.4/build_all.bat
+++
@@ -1,6 +0,0 @@
-for %%f in (vars-alpine-3.4-*.json) do (
-  for %%g in (alpine-3.4-minimal.json alpine-3.4-ansible.json) do (
-    packer build -var-file=%%f %* %%g
-  )
-)
-packer build -var-file=vars-alpine-3.4-x86_64.json %* alpine-3.4-docker.json 
