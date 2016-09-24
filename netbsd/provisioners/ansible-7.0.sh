004eba6086817d3942e9fd08dd6e5b09e967ee88
upperstream <developer@upperstream.jp>
2016-09-24 19:59:10 +0900

    NetBSD: Add templates for NetBSD 7.0.1 (Resolves #31)

--- netbsd/provisioners/ansible-7.0.sh
+++ netbsd/provisioners/ansible-7.0.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -ex
-pkg_add python27-2.7.11 py27-pip-7.1.2
+test -z "$PYTHON" && PYTHON=python27-2.7.11
 test -z "$ANSIBLE_VERSION" && ANSIBLE_VERSION=2.1.0.0
 test -z "$TESTINFRA_VERSION" && TESTINFRA_VERSION=1.2.0
+
+pkg_add $PYTHON py27-pip-7.1.2
 pip2.7 install ansible==$ANSIBLE_VERSION testinfra==$TESTINFRA_VERSION
