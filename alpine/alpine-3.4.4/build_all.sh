c407c1a37ebe3464dc77ae6662b68368c78059cc
upperstream <developer@upperstream.jp>
2016-10-03 08:26:11 +0900

    Alpine 3.4.4: Support kernels optimised for virtual machines (Resolves #45)

--- alpine/alpine-3.4.4/build_all.sh
+++ alpine/alpine-3.4.4/build_all.sh
@@ -5,3 +5,10 @@ for f in vars-alpine-3.4.4-*.json; do
   done
 done
 packer build -var-file=vars-alpine-3.4.4-x86_64.json $* alpine-3.4.4-docker.json 
+
+for f in vars-alpine-virt-3.4.4-*.json; do
+  for g in alpine-3.4.4-*.json; do
+    packer build -var-file=$f $* $g
+  done
+done
+packer build -var-file=vars-alpine-virt-3.4.4-x86_64.json $* alpine-3.4.4-docker.json 
