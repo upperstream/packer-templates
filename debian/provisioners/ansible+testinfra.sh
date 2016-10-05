f367c17a272143729ccb7ce9b7166ddadd90206e
upperstream <developer@upperstream.jp>
2016-10-06 02:34:00 +0900

    Debian: Upgrade Ansible from v2.1.1.0 to v2.1.2.0

--- debian/provisioners/ansible+testinfra.sh
+++ debian/provisioners/ansible+testinfra.sh
@@ -1,22 +1,22 @@
 #!/bin/sh -ex
 test -z "$PYTHON_DEV" && PYTHON_DEV=python-dev=2.7.3-4+deb7u1
 test -z "$LIBFFI_DEV" && LIBFFI_DEV=libffi-dev=3.0.10-3
-test -z "$LIBYAML_DEV" && LIBYAML_DEV=libyaml-dev=0.1.6-3
-test -z "$LIBGMP_DEV" && LIBGMP_DEV=libgmp-dev=2:6.0.0+dfsg-6
-test -z "$LIBSSL_DEV" && LIBGMP_DEV=libssl-dev=1.0.1t-1+deb8u3
-test -z "$PYTHON_PIP" && PYTHON_PIP=1.1-3
+test -z "$LIBYAML_DEV" && LIBYAML_DEV=libyaml-dev=0.1.4-2+deb7u5
+test -z "$LIBGMP_DEV" && LIBGMP_DEV=libgmp-dev=2:5.0.5+dfsg-2
+test -z "$LIBSSL_DEV" && LIBSSL_DEV=libssl-dev=1.0.1e-2+deb7u20
+test -z "$ANSIBLE" && ANSIBLE=ansible
+test -z "$TESTINFRA" && TESTINFRA=testinfra
+
 apt-get -y install $PYTHON_DEV $LIBFFI_DEV $LIBYAML_DEV $LIBGMP_DEV $LIBSSL_DEV
-apt-get -y install $PYTHON_PIP
-pip install --upgrade setuptools cryptography
-if [ -z "$ANSIBLE_VERSION" ]; then
-  pip install ansible
+wget -O - https://bootstrap.pypa.io/get-pip.py | python
+test -n "$PIP" && pip install --upgrade $PIP
+pip --version
+if [ -z "$CFFI_VERSION" ]; then
+  pip install cffi
 else
-  pip install ansible==$ANSIBLE_VERSION
+  pip install cffi==$CFFI_VERSION
 fi
-if [ -z "$TESTINFRA_VERSION" ]; then
-  pip install testinfra
-else
-  pip install testinfra==$TESTINFRA_VERSION
-fi
+pip install --upgrade cryptography
+pip install $ANSIBLE $TESTINFRA
 apt-get -y remove python-dev libffi-dev libyaml-dev libgmp-dev libssl-dev
 apt-get -y autoremove
