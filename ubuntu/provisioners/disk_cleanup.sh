a4d8590706b58336866d18183b4a43c919013c75
upperstream <developer@upperstream.jp>
2017-04-30 12:08:34 +0900

    Ubuntu 16.04: Upgrade Ubuntu 16.04 to 16.04.1 (#34)

--- ubuntu/provisioners/disk_cleanup.sh
+++ ubuntu/provisioners/disk_cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "$(date): Filling the remaining disk space with 0x00.  This will take long (appr. 15 minutes per 100GB)."
-sudo dd if=/dev/zero of=/tmp/zero.bin
+sudo dd if=/dev/zero of=/tmp/zero.bin || true
 sudo rm /tmp/zero.bin
 echo "$(date): Disk cleanup completed."
