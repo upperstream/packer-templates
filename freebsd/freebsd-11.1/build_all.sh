31d724547903598fb7b45475d91ca438f52b8871
upperstream <developer@upperstream.jp>
2017-06-30 06:05:00 +0900

    FreeBSD 11.1: Upgrade to FreeBSD 11.1-BETA3 (#65)

--- freebsd/freebsd-11.1/build_all.sh
+++ freebsd/freebsd-11.1/build_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-for f in freebsd-11.1-beta-*.json; do packer build -var-file=vars-freebsd-11.1-amd64.json "$@" $f; done
-for f in freebsd-11.1-beta-[^d]*.json; do \
-  grep docker $f || packer build -var-file=vars-freebsd-11.1-i386.json "$@" $f; \
+for f in freebsd-11.1-beta-*.json; do packer build "$@" $f; done
+for f in freebsd-11.1-beta-*.json; do
+  grep docker $f > /dev/null || packer build -var-file=vars-freebsd-11.1-i386.json "$@" $f;
 done
--- freebsd/provisioners/docker-11.sh
+++ freebsd/provisioners/docker-11.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-pkg install -y docker-freebsd-20150625
+pkg install -y ${DOCKER_FREEBSD:-"docker-freebsd-20150625"}
 echo "fdesc                   /dev/fd         fdescfs rw              0       0" >> /etc/fstab
 mount fdesc
 zfs create -o mountpoint=/usr/docker zroot/docker
@@ -10,5 +10,5 @@ mkdir -p /usr/local/bin
 docker run --rm jpetazzo/nsenter cat /nsenter > /usr/local/bin/nsenter && chmod +x /usr/local/bin/nsenter && strip /usr/local/bin/nsenter
 fetch -o /usr/local/bin/docker-enter https://raw.githubusercontent.com/jpetazzo/nsenter/master/docker-enter
 chmod +x /usr/local/bin/docker-enter
-pkg install -y py27-pip-8.0.2
-pip install docker-compose==1.3.3
+pkg install -y ${PY27_PIP:-"py27-pip-8.0.2"}
+pip install ${DOCKER_COMPOER:-"docker-compose==1.3.3"}
