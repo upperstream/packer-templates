bc7b2ed727c54a50f4df0915368ef2b63e925d04
upperstream <developer@upperstream.jp>
2017-07-01 13:12:47 +0900

    CentOS 7: Add a template supporting Ansible + Testinfra

--- centos/provisioners/epel-7-x86_64.sh
+++ centos/provisioners/epel-7-x86_64.sh
@@ -1,6 +1,2 @@
 #!/bin/sh -ex
-# EPEL
-if ! (md5sum epel-release-7-8.noarch.rpm | grep 8c2d7a86160d3f55f8d45730d325d0a6); then
-  curl -o /tmp/epel-release-7-8.noarch.rpm http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
-fi
-rpm -ivh /tmp/epel-release-7-8.noarch.rpm
+rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
