4cdbc8abc030237926d93be7031e6a93f1aedb26
upperstream <developer@upperstream.jp>
2017-07-18 20:04:43 +0900

    Devuan 1.0: Installer DVD can be used

--- devuan/provisioners/base.sh
+++ devuan/provisioners/base.sh
@@ -1,3 +1,17 @@
-#!/bin/sh -ex
-apt-get -y install rsync
+#!/bin/sh
+set -e
+set -x
+
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+if [ ${INSTALL_FROM_DVD:-false} = "true" ]; then
+  sed -e '/^#deb cdrom:.*$/a\
+\
+deb http://auto.mirror.devuan.org/merged jessie main\
+deb-src http://auto.mirror.devuan.org/merged jessie main' \
+      -e 's/^# deb http:/deb http:/' \
+      -e 's/^# deb-src http:/deb-src http:/' /etc/apt/sources.list > /tmp/sources.list
+  mv /tmp/sources.list /etc/apt/sources.list
+  apt-get update
+fi
+apt-get -y install rsync
