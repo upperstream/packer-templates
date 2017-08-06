21acaacaf781d804be59b25d3abb6d72830c441a
upperstream <developer@upperstream.jp>
2017-08-07 04:37:55 +0900

    NetBSD 7.x: Add `/usr/sbin` to the user's $PATH during non-interactive SSH session (#88)
    This allows Vagrant to locate admin commands such as `chown` and
    `pkg_add`.

--- netbsd/provisioners/vagrant-7.0.sh
+++ netbsd/provisioners/vagrant-7.0.sh
@@ -8,10 +8,15 @@ mkdir -p /home/$VAGRANT_USER/.ssh
 ftp -o - "https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub" >> /home/$VAGRANT_USER/.ssh/authorized_keys
 chown -R $VAGRANT_USER:$VAGRANT_GROUP /home/$VAGRANT_USER
 chmod -R og-rwx /home/$VAGRANT_USER/.ssh
+echo "PATH=/usr/bin:/bin:/usr/pkg/bin:/usr/local/bin:/usr/sbin" > /home/$VAGRANT_USER/.ssh/environment
+chown root:$VAGRANT_GROUP /home/$VAGRANT_USER/.ssh/environment
+chmod 0640 /home/$VAGRANT_USER/.ssh/environment
+
 sed \
   -e 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' \
   -e 's/^UsePam yes/UsePam no/' \
   -e 's/^#UseDNS no/UseDNS no/' \
+  -e 's/^#PermitUserEnvironment no/PermitUserEnvironment yes/' \
   -e 's/^#NoneEnabled no/NoneEnabled yes/' /etc/ssh/sshd_config > /tmp/sshd_config
 mv /tmp/sshd_config /etc/ssh/sshd_config
 
