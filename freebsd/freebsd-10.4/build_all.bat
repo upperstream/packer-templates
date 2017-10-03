cbce3b4c8607f4d5ba135857daa3df073fb0bb2c
upperstream <developer@upperstream.io>
2017-10-04 05:52:34 +0900

    FreeBSD 10.4: Upgrade to FreeBSD 10.4-RELEASE (#70)

--- freebsd/freebsd-10.4/build_all.bat
+++ freebsd/freebsd-10.4/build_all.bat
@@ -1,6 +1,6 @@
-for %%f in (freebsd-10.4-rc-*.json) do packer build %* %%f
-for %%f in (freebsd-10.4-rc-*.json) do (
-  if not "%%f"=="freebsd-10.4-rc-docker.json" (
-    if  not "%%f"=="freebsd-10.4-rc-zfs.json" packer build -var-file=vars-freebsd-10.4-i386.json %* %%f
+for %%f in (freebsd-10.4-release-*.json) do packer build %* %%f
+for %%f in (freebsd-10.4-release-*.json) do (
+  if not "%%f"=="freebsd-10.4-release-docker.json" (
+    if  not "%%f"=="freebsd-10.4-release-zfs.json" packer build -var-file=vars-freebsd-10.4-i386.json %* %%f
   )
 )
