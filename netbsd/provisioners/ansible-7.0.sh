fb96536b4abe35b19fe3adfb7884cbc97218a86a
upperstream <developer@upperstream.io>
2017-12-31 12:20:36 +0900

    NetBSD 7.1: Upgrade to NetBSD 7.1.1 (#109)

--- netbsd/provisioners/ansible-7.0.sh
+++ netbsd/provisioners/ansible-7.0.sh
@@ -1,8 +1,3 @@
 #!/bin/sh -ex
-test -z "$PYTHON" && PYTHON=python27-2.7.11
-test -z "$PY27_PIP" && PY27_PIP=py27-pip-7.1.2
-test -z "$ANSIBLE_VERSION" && ANSIBLE_VERSION=2.1.0.0
-test -z "$TESTINFRA_VERSION" && TESTINFRA_VERSION=1.2.0
-
-pkg_add $PYTHON $PY27_PIP
-pip2.7 install ansible==$ANSIBLE_VERSION testinfra==$TESTINFRA_VERSION
+pkg_add ${PYTHON:-python27-2.7.11} ${PY27_PIP:-py27-pip-7.1.2}
+pip2.7 install ansible==${ANSIBLE_VERSION:-2.1.0.0} testinfra==${TESTINFRA_VERSION:-1.2.0}
