be2e86e3322c5a50ebc199406e8b587d27691c05
upperstream <developer@upperstream.io>
2019-06-16 02:39:42 +0900

    FreeBSD 11.3: Upgrade to FreeBSD 11.3-RC1 (#135)

--- freebsd/freebsd-11.3/build_all.sh
+++ freebsd/freebsd-11.3/build_all.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -x
-for f in freebsd-11.3-beta-*.json; do packer build "$@" $f; done
-for f in freebsd-11.3-beta-*.json; do
-  grep docker $f > /dev/null || packer build -var-file=vars-freebsd-11.3-i386.json "$@" $f;
-done
+for f in freebsd-11.3-rc-*.json; do packer build "$@" $f; done
+for f in freebsd-11.3-rc-*.json; dopacker build -var-file=vars-freebsd-11.3-i386.json "$@" $f; done
