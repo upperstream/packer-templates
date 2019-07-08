6da24a50eddf6f2d9e705b8efde6ed9f326b9c63
upperstream <developer@upperstream.io>
2019-07-09 02:36:25 +0900

    Debian: Add templates for Debian 10.0 (#139)

--- debian/provisioners/virtualbox.sh
+++ debian/provisioners/virtualbox.sh
@@ -3,9 +3,20 @@ set -e
 set -x
 
 test "${INSTALL_DKMS:-false}" = "true" && apt-get -y install dkms
-test -z "$VBOX_VER" && VBOX_VER=`cat /root/.vbox_version`
+if [ -z "$VBOX_VER" ]; then
+    VBOX_VER=`cat /root/.vbox_version`
+fi
 test "$VIRTUALBOX_WITH_XORG" = "1" || VIRTUALBOX_WITHOUT_XORG=--nox11
 wget http://download.virtualbox.org/virtualbox/$VBOX_VER/VBoxGuestAdditions_$VBOX_VER.iso
 mount -t iso9660 VBoxGuestAdditions_$VBOX_VER.iso /media/cdrom0
-yes | /media/cdrom0/VBoxLinuxAdditions.run $VIRTUALBOX_WITHOUT_XORG
+if { yes | /media/cdrom0/VBoxLinuxAdditions.run "$@" $VIRTUALBOX_WITHOUT_XORG; }; then
+    :
+else
+    rc="$?"
+    if [ "$rc" = "2" ]; then
+        echo "Assume VirtualBox Guest Additions should work after reboot and ignore this error."
+    else
+        exit $rc
+    fi
+fi
 rm -f VBoxGuestAdditions_*.iso
