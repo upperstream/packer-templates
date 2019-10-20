ddc202f35e5f647b5ff89057f9f8e03e9d1b5b74
upperstream <developer@upperstream.io>
2019-10-21 00:01:04 +0900

    FreeBSD 12.1: Upgrade to FreeBSD 12.1-RC1 (#136)

--- freebsd/freebsd-12.1/build_all.bat
+++ freebsd/freebsd-12.1/build_all.bat
@@ -1,4 +1,3 @@
-for %%f in (freebsd-12.1-release-*.json) do packer build -var-file=vars-freebsd-12.1-amd64.json %* %%f
-for %%f in (freebsd-12.1-release-*.json) do (
-  if not "%%f"=="freebsd-12.1-release-docker.json" packer build -var-file=vars-freebsd-12.1-i386.json %* %%f
+for %%f in (freebsd-12.1-rc-*.json) do packer build -var-file=vars-freebsd-12.1-amd64.json %* %%f
+for %%f in (freebsd-12.1-rc-*.json) do packer build -var-file=vars-freebsd-12.1-i386.json %* %%f
 )
