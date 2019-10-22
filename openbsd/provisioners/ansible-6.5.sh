7e06c87f9cfcdfd18dfcf118adb95b56d34f978c
upperstream <developer@upperstream.io>
2019-10-22 11:15:51 +0900

    OpenBSD 6.5: Correct variable name for Ansible Lint
    Version pinning for Ansible Lint package did not work because
    environment variable name was misspelt.

--- openbsd/provisioners/ansible-6.5.sh
+++ openbsd/provisioners/ansible-6.5.sh
@@ -2,5 +2,5 @@
 set -e
 set -x
 
-pkg_add ansible-${ANSIBLE:-2.7.9} py3-pip-${PY3_PIP:-9.0.3} ansible-lint-${ANBILE_LINT:-4.1.0}
+pkg_add ansible-${ANSIBLE:-2.7.9} py3-pip-${PY3_PIP:-9.0.3} ansible-lint-${ANSIBLE_LINT:-4.1.0}
 pip3.6 install testinfra==${TESTINFRA:-2.0.0}
