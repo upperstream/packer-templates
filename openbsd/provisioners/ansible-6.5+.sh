078b9457780fe474cdb2c64cb8992add865bf7d1
upperstream <developer@upperstream.io>
2019-10-22 12:01:39 +0900

    OpenBSD 6.5: Rename provisioner name (#140)
    - ansible-6.5.sh -> ansible-6.5+.sh
    - x11-6.5.sh -> x11-6.5+.sh

--- openbsd/provisioners/ansible-6.5+.sh
+++ openbsd/provisioners/ansible-6.5+.sh
@@ -3,4 +3,5 @@ set -e
 set -x
 
 pkg_add ansible-${ANSIBLE:-2.7.9} py3-pip-${PY3_PIP:-9.0.3} ansible-lint-${ANSIBLE_LINT:-4.1.0}
-pip3.6 install testinfra==${TESTINFRA:-2.0.0}
+PIP=pip`python3 --version | cut -f2 -d' ' | cut -f 1 -f 2 -d'.'`
+$PIP install testinfra==${TESTINFRA:-2.0.0}
