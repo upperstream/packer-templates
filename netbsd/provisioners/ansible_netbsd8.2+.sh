ef94a8d743ac229392732e98d5e7786b59f8f1a6
upperstream <developer@upperstream.io>
2020-05-06 11:49:18 +0900

    NetBSD 9.0: Rename environment variables for package versions (#152)
    - `ANSIBLE_LINT` -> `ANSIBLE_LINT_VERSION`
    - `TESTINFRA` -> `TESTINFRA_VERSION`

--- netbsd/provisioners/ansible_netbsd8.2+.sh
+++ netbsd/provisioners/ansible_netbsd8.2+.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 
 pkg_add ${PYTHON:-python37} ${PYTHON_PIP:-py37-pip} ${ANSIBLE:-ansible}
-${PIP:=python3.7 -m pip} install ${TESTINFRA:-testinfra}
-if [ "$ANSIBLE_LINT" ]; then
-    $PIP install "$ANSIBLE_LINT"
+${PIP:=python3.7 -m pip} install testinfra==${TESTINFRA_VERSION:-4.1.0}
+if [ "$ANSIBLE_LINT_VERSION" ]; then
+    $PIP install ansible-lint=="$ANSIBLE_LINT_VERSION"
 fi
