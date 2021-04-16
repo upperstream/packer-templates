aefa23159bd156b9a455f465b29047586186a80b
upperstream <developer@upperstream.io>
2021-04-17 00:59:43 +0900

    FreeBSD 13.0: Upgrade to FreeBSD 13.0-RELEASE (GH-162)

--- freebsd/freebsd-13.0/build_all.bat
+++ freebsd/freebsd-13.0/build_all.bat
@@ -1,2 +1,2 @@
-for %%f in (freebsd-13.0-rc-*.json) do packer build %* %%f
-for %%f in (freebsd-13.0-rc-*.json) do packer build -var-file=vars-freebsd-13.0-i386.json %* %%f
+for %%f in (freebsd-13.0-release-*.json) do packer build %* %%f
+for %%f in (freebsd-13.0-release-*.json) do packer build -var-file=vars-freebsd-13.0-i386.json %* %%f
--- freebsd/freebsd-13.0/build_all.sh
+++ freebsd/freebsd-13.0/build_all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -x
-for f in freebsd-13.0-rc-*.json; do packer build "$@" $f; done
-for f in freebsd-13.0-rc-*.json; do packer build -var-file=vars-freebsd-13.0-i386.json "$@" $f; done
+for f in freebsd-13.0-release-*.json; do packer build "$@" $f; done
+for f in freebsd-13.0-release-*.json; do packer build -var-file=vars-freebsd-13.0-i386.json "$@" $f; done
