66a8b87526f208ec8487a3b80c0cc92b9688864e
upperstream <developer@upperstream.io>
2022-04-23 20:52:05 +0900

    OpenBSD 7.1: Upgrade to OpenBSD 7.1 (GH-208)

--- openbsd/provisioners/dwm+dmenu+st_openbsd7.sh
+++ openbsd/provisioners/dwm+dmenu+st_openbsd7.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 set -x
-pkg_add "${SLIM:-slim}" "${SLIM_THEMES:-slim-themes}" "${DWM:-dwm}" "${DMENU:-dmenu}" "${ST:-st}"
-echo '/usr/local/bin/slim -d' >> /etc/rc.local
 
 cat >> /etc/rc.conf.local << EOF
 pkg_scripts="dbus_daemon avahi_daemon"
@@ -10,9 +8,23 @@ dbus_enable=YES
 multicast_host=YES
 hotplugd_flags=""    ## needed to start hotplugd for toad
 apmd_flags="-A"      ## enables suspend and power mgmt
-# messagebus is the new name for dbus_daemon in 5.8
-pkg_scripts="${pkg_scripts} messagebus toadd slim"
 EOF
+
+if [ "$SLIM" ]; then
+	pkg_add "${SLIM:-slim}" "${SLIM_THEMES:-slim-themes}"
+	echo '/usr/local/bin/slim -d' >> /etc/rc.local
+	echo 'pkg_scripts="${pkg_scripts} messagebus toadd slim"' >> /etc/rc.conf.local
+	sed -i.orig '/current_theme/s/default/openbsd-simple/' /etc/slim.conf
+else
+	rcctl enable xenodm
+	rcctl start xenodm
+	sed 's/^XConsole\(.*\)/!XConsole\1/' /etc/X11/xenodm/Xresources > /tmp/Xresources
+	mv /tmp/Xresources /etc/X11/xenodm/Xresources
+	sed 's/^DisplayManager\._0\(.*\)/!DisplayManager._0\1/' /etc/X11/xenodm/xenodm-config > /tmp/xenodm-config
+	mv /tmp/xenodm-config /etc/X11/xenodm/xenodm-config
+	echo "DisplayManager*authName:        MIT-MAGIC-COOKIE-1" >> /etc/X11/xenodm/xenodm-config
+fi
+pkg_add "${DWM:-dwm}" "${DMENU:-dmenu}" "${ST:-st}"
 
 cat << EOF > "/home/$VAGRANT_USER/.xinitrc"
 st &
@@ -24,5 +36,3 @@ st &
 exec dwm
 EOF
 chown "$VAGRANT_USER" "/home/$VAGRANT_USER/.xinitrc" "/home/$VAGRANT_USER/.xsession"
-
-sed -i.orig '/current_theme/s/default/openbsd-simple/' /etc/slim.conf
